268e1d0599d13cea89a40f3b92f113c12a490b1f
Michael <m.a.nikityuk@gmail.com>
2025-05-12 10:47:48 +0300

    Change layout

--- grok/frontend/src/components/layout/Layout.tsx
+++ grok/frontend/src/components/layout/Layout.tsx
@@ -9,8 +9,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-white text-gray-800 flex flex-col">
       <Header />
-      <main className="flex-grow p-6">
-        <div className="container mx-auto max-w-12xl">{children}</div>
+      <main className="flex-grow p-6 overflow-hidden">
+        <div className="container mx-auto max-w-12xl h-full">{children}</div>
       </main>
     </div>
   );
